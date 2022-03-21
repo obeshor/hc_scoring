52c3c6d92aec8937d04844d457082924da399987
aschoonheere <98112581+aschoonheere@users.noreply.github.com>
2022-03-21 18:00:13 +0100

    Delete hc_scoring directory

--- hc_scoring/plotlydash/data.py
+++
@@ -1,9 +0,0 @@
-"""Prepare data for Plotly Dash."""
-import numpy as np
-import pandas as pd
-
-
-def create_dataframe():
-    """Create Pandas DataFrame from local CSV."""
-    df = pd.read_csv("data/HomeCredit_columns_description.csv")
-    return df
